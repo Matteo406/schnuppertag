2305ab3be5d347c07e525d3dfb280fb754cc2181
Tim Paris <tim.paris@bossinfo.ch>
2023-11-15 08:35:26 +0100

    update

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-function showSolution(exerciseId) {
-  console.log("exerciseId", exerciseId);
-  const solutionDiv = document.getElementById(`solution${exerciseId}`);
-
-  if (!showSolution === null ) {
-    solutionDiv?.style.display =
-        solutionDiv.style.display === "none" ? "block" : "none";
+document.addEventListener("DOMContentLoaded", function () {
+  function showSolution(exerciseId) {
+    const solutionDiv = document.getElementById(`solution${exerciseId}`);
+    solutionDiv.style.display =
+      solutionDiv.style.display === "none" ? "block" : "none";
   }
-  
-}
+
+  // Attach click event handlers
+  document
+    .getElementById("exercise1-btn")
+    .addEventListener("click", function () {
+      showSolution("exercise1");
+    });
+
+  document
+    .getElementById("exercise2-btn")
+    .addEventListener("click", function () {
+      showSolution("exercise2");
+    });
+
+  document
+    .getElementById("exercise3-btn")
+    .addEventListener("click", function () {
+      showSolution("exercise3");
+    });
+});
