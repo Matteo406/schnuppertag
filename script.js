ade8544ebdd342e9ea8cf19944281128a37f5db0
Tim Paris <tim.paris@bossinfo.ch>
2023-11-15 08:29:43 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function showSolution(exerciseId) {
+  console.log("exerciseId", exerciseId);
   const solutionDiv = document.getElementById(`solution${exerciseId}`);
   solutionDiv.style.display =
     solutionDiv.style.display === "none" ? "block" : "none";
